f1cc16da7a0269fb207719ec9d5e0fa5631d7c13
shinheekim <shinhee1241@gmail.com>
2024-11-20 09:07:01 +0900

    feat(todo): todo 조회 시 날짜, 생성일 기간 조회

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -12,6 +12,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequiredArgsConstructor
 public class TodoController {
@@ -29,9 +31,12 @@ public class TodoController {
     @GetMapping("/todos")
     public ResponseEntity<Page<TodoResponse>> getTodos(
             @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size
-    ) {
-        return ResponseEntity.ok(todoService.getTodos(page, size));
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) String weather,
+            @RequestParam(required = false) LocalDateTime startCreatedAt,
+            @RequestParam(required = false) LocalDateTime endCreatedAt
+            ) {
+        return ResponseEntity.ok(todoService.getTodos(page, size, weather, startCreatedAt, endCreatedAt));
     }
 
     @GetMapping("/todos/{todoId}")
--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,9 +7,24 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+    @Query("""
+        SELECT t FROM Todo t
+        WHERE (:weather IS NULL OR t.weather = :weather)
+        AND (:startCreatedAt IS NULL OR t.createdAt >= :startCreatedAt)
+        AND (:endCreatedAt IS NULL OR t.createdAt <= :endCreatedAt)
+        ORDER BY t.modifiedAt DESC
+    """)
+    Page<Todo> findAllByWeatherAndCreatedAtRange(
+            @Param("weather") String weather,
+            @Param("startCreatedAt") LocalDateTime startCreatedAt,
+            @Param("endCreatedAt") LocalDateTime endCreatedAt,
+            Pageable pageable
+    );
 
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
